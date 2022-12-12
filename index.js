2881242332b0d3d1c1f72a5dfbe281ebb83dc012
vladp7ay3r <khalipov.vlad@gmail.com>
2022-12-12 03:29:00 +0300

    create server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import {WeatherService} from "./services/weatherService.js";
-import {WeatherResource} from "./services/weatherResource.js";
+import { WeatherService } from './services/weatherService.js';
+import { WeatherResource } from './services/weatherResource.js';
 
 const form = document.querySelector('.weather__form');
 
-const  main = async () => {
-  const weatherResource = new WeatherResource('https://fcc-weather-api.glitch.me');
+const main = async () => {
+  const weatherResource = new WeatherResource('http://localhost:5000');
   const weatherService = new WeatherService();
   const data = await weatherResource.get();
 
@@ -12,12 +12,11 @@ const  main = async () => {
   weatherService.showCity(data.city);
   weatherService.showTemperature(data.temperature);
   weatherService.showDescription(data.description);
-}
+};
 // Погода  после загрузки страницы
-window.addEventListener('load',main);
+window.addEventListener('load', main);
 
-form.addEventListener('submit', (event) =>{
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   main();
-})
-
+});
--- services/weatherResource.js
+++ services/weatherResource.js
@@ -1,25 +1,27 @@
-import {citiesCoords} from '../constants.js';
+import { citiesCoords } from '../constants.js';
 
-export class WeatherResource{
-	constructor(baseUrl){
-		this.baseUrl = baseUrl;
-	}
-	
-	async get(){
-		 const selectedCity = document.querySelector('.weather__select').value;
-		 const selectedCityCoords = citiesCoords[selectedCity];
-	  try{
-		 const response = await fetch(`${this.baseUrl}/api/current?lat=${selectedCityCoords.latitude}&lon=${selectedCityCoords.longitude}`);
-		 const data = await response.json();
-		 return {
-			'country' : data.sys.country,
-			'city' : data.name,
-			'temperature' : data.main.temp,
-			'description' : data.weather[0].main
-		 };
-	  }catch(err){
-		 console.error(err);
-	  }
-	}
- }
+export class WeatherResource {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
 
+  async get() {
+    const selectedCity = document.querySelector('.weather__select').value;
+    const selectedCityCoords = citiesCoords[selectedCity];
+    try {
+      //Отправляю на сервер (localhost) долготу и широту выбранного города
+      const response = await fetch(
+        `${this.baseUrl}?lat=${selectedCityCoords.latitude}&lon=${selectedCityCoords.longitude}`,
+      );
+      const data = await response.json();
+      return {
+        country: data.sys.country,
+        city: data.name,
+        temperature: data.main.temp,
+        description: data.weather[0].main,
+      };
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
