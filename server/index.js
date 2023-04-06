979f1c0c3b2f4d2c182660bcc67570292a67384d
vladp7ay3r <khalipov.vlad@gmail.com>
2023-04-06 04:41:08 +0300

    remove from server folder env and package.json

--- server/index.js
+++ server/index.js
@@ -17,57 +17,6 @@ const DB_NAME = process.env.DB_NAME;
 app.use(cors());
 app.use('/api/weather', weatherRoute);
 
-// async function handleRequest(req, res) {
-// 	const collection = req.app.locals.collection;
-// 	const { lat, lon } = req.query;
-// 	const requestService = new RequestService();
-// 	const coord = {
-// 		lon: Number(lon),
-// 		lat: Number(lat),
-// 	};
-// 	try {
-// 		const response = await requestService.getData(lat, lon);
-// 		// Получаем объект города из бд, если он там есть
-// 		const cityWeatherInfo = await collection.findOne({
-// 			coord,
-// 		});
-// 		//Если города в бд нет, то делаем запрос к API
-// 		if (!cityWeatherInfo) {
-// 			//Использовал свою широту и долготу из client/constants.js, т.к немного отличались от API
-// 			console.log('added to mongo' + ' ' + new Date());
-// 			await collection.insertOne({
-// 				...response,
-// 				coord,
-// 				requestDate: Date.now(),
-// 			});
-// 			res.send(response);
-// 		} else {
-// 			// Если прошло больше 5 минут
-// 			// Запрос к API за новыми данными
-// 			if (timeDifference(cityWeatherInfo.requestDate) > 1) {
-// 				console.log('More than 1 minute' + ' ' + new Date());
-// 				await collection.findOneAndUpdate(
-// 					{
-// 						coord,
-// 					},
-// 					{
-// 						$set: {
-// 							...response,
-// 							coord,
-// 							requestDate: Date.now(),
-// 						},
-// 					},
-// 				);
-// 				return res.send(response);
-// 			}
-// 			console.log('Less than 1 minute' + ' ' + new Date());
-// 			res.send(cityWeatherInfo);
-// 		}
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-// }
-
 async function start() {
 	try {
 		await mongoose.connect(
